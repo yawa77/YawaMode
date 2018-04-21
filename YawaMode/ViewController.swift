3e6da3f3750038d90f7d04f121fd6e7573cee2d5
David Pollard <dsepollard@gmail.com>
2018-04-21 00:19:35 -0500

    Initial comment

---
+++ YawaMode/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  YawaMode
+//
+//  Created by David Pollard on 2/7/18.
+//  Copyright © 2018 David Pollard. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var darkBlueBG: UIImageView!
+    @IBOutlet weak var powerButton: UIButton!
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+   
+
+
+}
+
